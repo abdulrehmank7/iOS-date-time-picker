a6eb9b6058bfe988f57c654d4b5ab0d1663bae56
Abdul Rehman <abdulrehman0796@gmail.com>
2020-10-28 22:20:18 +0530

    Updates: - Bug fixes.

--- app/iosDatetTimePicker/src/main/java/com/arkapp/iosdatettimepicker/utils/DateViewHolder.kt
+++ app/iosDatetTimePicker/src/main/java/com/arkapp/iosdatettimepicker/utils/DateViewHolder.kt
@@ -1,5 +1,6 @@
 package com.arkapp.iosdatettimepicker.utils
 
+import androidx.annotation.Keep
 import androidx.recyclerview.widget.RecyclerView
 import com.arkapp.iosdatettimepicker.databinding.RecyclerDateSeletionBinding
 
@@ -7,5 +8,6 @@ import com.arkapp.iosdatettimepicker.databinding.RecyclerDateSeletionBinding
  * Created by Abdul Rehman on 12-03-2020.
  * Contact email - abdulrehman0796@gmail.com
  */
+@Keep
 class DateViewHolder(val binding: RecyclerDateSeletionBinding) :
     RecyclerView.ViewHolder(binding.root)
--- app/iosDatetTimePicker/src/main/java/com/arkapp/iosdatettimepicker/utils/OnDateTimeSelectedListener.kt
+++ app/iosDatetTimePicker/src/main/java/com/arkapp/iosdatettimepicker/utils/OnDateTimeSelectedListener.kt
@@ -1,11 +1,13 @@
 package com.arkapp.iosdatettimepicker.utils
 
+import androidx.annotation.Keep
 import java.util.*
 
 /**
  * Created by Abdul Rehman on 01-04-2020.
  * Contact email - abdulrehman0796@gmail.com
  */
+@Keep
 interface OnDateTimeSelectedListener {
     fun onDateTimeSelected(selectedDateTime: Calendar)
 }
